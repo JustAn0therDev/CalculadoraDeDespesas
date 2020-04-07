b339cc1fe51f5c5d64e4200d83fb6eb38564f05a
Ruan Montelo <ruanmontelo@gmail.com>
2020-04-07 19:58:07 -0300

    First Commit for the CalculadoraDeDespesas project

---
+++ CalculadoraDeDespesas/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace CalculadoraDeDespesas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AlmiraLabel = new System.Windows.Forms.Label();
+            this.CarlosAlbertoLabel = new System.Windows.Forms.Label();
+            this.RuanLabel = new System.Windows.Forms.Label();
+            this.GroupBoxOfResults = new System.Windows.Forms.GroupBox();
+            this.TotalIncome = new System.Windows.Forms.Label();
+            this.TotalIncomeLabel = new System.Windows.Forms.Label();
+            this.TotalSpendings = new System.Windows.Forms.Label();
+            this.TotalSpendingsLabel = new System.Windows.Forms.Label();
+            this.TotalOfVariableSpendingsTextBox = new System.Windows.Forms.Label();
+            this.TotalOfFixedSpendingsTextBox = new System.Windows.Forms.Label();
+            this.TotalOfVariableSpendingsLabel = new System.Windows.Forms.Label();
+            this.TotalOfFixedSpendingsLabel = new System.Windows.Forms.Label();
+            this.AlmiraTextBox = new System.Windows.Forms.TextBox();
+            this.CarlosTextBox = new System.Windows.Forms.TextBox();
+            this.RuanTextBox = new System.Windows.Forms.TextBox();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.GroupBoxOfResults.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AlmiraLabel
+            // 
+            this.AlmiraLabel.AutoSize = true;
+            this.AlmiraLabel.Location = new System.Drawing.Point(9, 92);
+            this.AlmiraLabel.Name = "AlmiraLabel";
+            this.AlmiraLabel.Size = new System.Drawing.Size(28, 13);
+            this.AlmiraLabel.TabIndex = 0;
+            this.AlmiraLabel.Text = "Mãe";
+            // 
+            // CarlosAlbertoLabel
+            // 
+            this.CarlosAlbertoLabel.AutoSize = true;
+            this.CarlosAlbertoLabel.Location = new System.Drawing.Point(9, 128);
+            this.CarlosAlbertoLabel.Name = "CarlosAlbertoLabel";
+            this.CarlosAlbertoLabel.Size = new System.Drawing.Size(22, 13);
+            this.CarlosAlbertoLabel.TabIndex = 1;
+            this.CarlosAlbertoLabel.Text = "Pai";
+            // 
+            // RuanLabel
+            // 
+            this.RuanLabel.AutoSize = true;
+            this.RuanLabel.Location = new System.Drawing.Point(9, 163);
+            this.RuanLabel.Name = "RuanLabel";
+            this.RuanLabel.Size = new System.Drawing.Size(33, 13);
+            this.RuanLabel.TabIndex = 2;
+            this.RuanLabel.Text = "Ruan";
+            // 
+            // GroupBoxOfResults
+            // 
+            this.GroupBoxOfResults.Controls.Add(this.TotalIncome);
+            this.GroupBoxOfResults.Controls.Add(this.TotalIncomeLabel);
+            this.GroupBoxOfResults.Controls.Add(this.TotalSpendings);
+            this.GroupBoxOfResults.Controls.Add(this.TotalSpendingsLabel);
+            this.GroupBoxOfResults.Controls.Add(this.TotalOfVariableSpendingsTextBox);
+            this.GroupBoxOfResults.Controls.Add(this.TotalOfFixedSpendingsTextBox);
+            this.GroupBoxOfResults.Controls.Add(this.TotalOfVariableSpendingsLabel);
+            this.GroupBoxOfResults.Controls.Add(this.TotalOfFixedSpendingsLabel);
+            this.GroupBoxOfResults.Location = new System.Drawing.Point(12, 221);
+            this.GroupBoxOfResults.Name = "GroupBoxOfResults";
+            this.GroupBoxOfResults.Size = new System.Drawing.Size(776, 217);
+            this.GroupBoxOfResults.TabIndex = 3;
+            this.GroupBoxOfResults.TabStop = false;
+            this.GroupBoxOfResults.Text = "Resultados";
+            // 
+            // TotalIncome
+            // 
+            this.TotalIncome.AutoSize = true;
+            this.TotalIncome.Location = new System.Drawing.Point(124, 45);
+            this.TotalIncome.Name = "TotalIncome";
+            this.TotalIncome.Size = new System.Drawing.Size(13, 13);
+            this.TotalIncome.TabIndex = 7;
+            this.TotalIncome.Text = "0";
+            // 
+            // TotalIncomeLabel
+            // 
+            this.TotalIncomeLabel.AutoSize = true;
+            this.TotalIncomeLabel.Location = new System.Drawing.Point(8, 45);
+            this.TotalIncomeLabel.Name = "TotalIncomeLabel";
+            this.TotalIncomeLabel.Size = new System.Drawing.Size(117, 13);
+            this.TotalIncomeLabel.TabIndex = 6;
+            this.TotalIncomeLabel.Text = "Total de Rendimentos: ";
+            // 
+            // TotalSpendings
+            // 
+            this.TotalSpendings.AutoSize = true;
+            this.TotalSpendings.Location = new System.Drawing.Point(124, 147);
+            this.TotalSpendings.Name = "TotalSpendings";
+            this.TotalSpendings.Size = new System.Drawing.Size(13, 13);
+            this.TotalSpendings.TabIndex = 5;
+            this.TotalSpendings.Text = "0";
+            // 
+            // TotalSpendingsLabel
+            // 
+            this.TotalSpendingsLabel.AutoSize = true;
+            this.TotalSpendingsLabel.Location = new System.Drawing.Point(8, 147);
+            this.TotalSpendingsLabel.Name = "TotalSpendingsLabel";
+            this.TotalSpendingsLabel.Size = new System.Drawing.Size(112, 13);
+            this.TotalSpendingsLabel.TabIndex = 4;
+            this.TotalSpendingsLabel.Text = "Total Atual de Gastos:";
+            // 
+            // TotalOfVariableSpendingsTextBox
+            // 
+            this.TotalOfVariableSpendingsTextBox.AutoSize = true;
+            this.TotalOfVariableSpendingsTextBox.Location = new System.Drawing.Point(143, 114);
+            this.TotalOfVariableSpendingsTextBox.Name = "TotalOfVariableSpendingsTextBox";
+            this.TotalOfVariableSpendingsTextBox.Size = new System.Drawing.Size(13, 13);
+            this.TotalOfVariableSpendingsTextBox.TabIndex = 3;
+            this.TotalOfVariableSpendingsTextBox.Text = "0";
+            // 
+            // TotalOfFixedSpendingsTextBox
+            // 
+            this.TotalOfFixedSpendingsTextBox.AutoSize = true;
+            this.TotalOfFixedSpendingsTextBox.Location = new System.Drawing.Point(126, 81);
+            this.TotalOfFixedSpendingsTextBox.Name = "TotalOfFixedSpendingsTextBox";
+            this.TotalOfFixedSpendingsTextBox.Size = new System.Drawing.Size(13, 13);
+            this.TotalOfFixedSpendingsTextBox.TabIndex = 2;
+            this.TotalOfFixedSpendingsTextBox.Text = "0";
+            // 
+            // TotalOfVariableSpendingsLabel
+            // 
+            this.TotalOfVariableSpendingsLabel.AutoSize = true;
+            this.TotalOfVariableSpendingsLabel.Location = new System.Drawing.Point(6, 114);
+            this.TotalOfVariableSpendingsLabel.Name = "TotalOfVariableSpendingsLabel";
+            this.TotalOfVariableSpendingsLabel.Size = new System.Drawing.Size(131, 13);
+            this.TotalOfVariableSpendingsLabel.TabIndex = 1;
+            this.TotalOfVariableSpendingsLabel.Text = "Total de Gastos Variáveis:";
+            // 
+            // TotalOfFixedSpendingsLabel
+            // 
+            this.TotalOfFixedSpendingsLabel.AutoSize = true;
+            this.TotalOfFixedSpendingsLabel.Location = new System.Drawing.Point(8, 81);
+            this.TotalOfFixedSpendingsLabel.Name = "TotalOfFixedSpendingsLabel";
+            this.TotalOfFixedSpendingsLabel.Size = new System.Drawing.Size(112, 13);
+            this.TotalOfFixedSpendingsLabel.TabIndex = 0;
+            this.TotalOfFixedSpendingsLabel.Text = "Total de Gastos Fixos:";
+            // 
+            // AlmiraTextBox
+            // 
+            this.AlmiraTextBox.Location = new System.Drawing.Point(48, 89);
+            this.AlmiraTextBox.Name = "AlmiraTextBox";
+            this.AlmiraTextBox.Size = new System.Drawing.Size(100, 20);
+            this.AlmiraTextBox.TabIndex = 4;
+            this.AlmiraTextBox.TextChanged += new System.EventHandler(this.AlmiraTextBox_TextChanged);
+            // 
+            // CarlosTextBox
+            // 
+            this.CarlosTextBox.Location = new System.Drawing.Point(48, 125);
+            this.CarlosTextBox.Name = "CarlosTextBox";
+            this.CarlosTextBox.Size = new System.Drawing.Size(100, 20);
+            this.CarlosTextBox.TabIndex = 5;
+            this.CarlosTextBox.TextChanged += new System.EventHandler(this.CarlosTextBox_TextChanged);
+            // 
+            // RuanTextBox
+            // 
+            this.RuanTextBox.Location = new System.Drawing.Point(48, 160);
+            this.RuanTextBox.Name = "RuanTextBox";
+            this.RuanTextBox.Size = new System.Drawing.Size(100, 20);
+            this.RuanTextBox.TabIndex = 6;
+            this.RuanTextBox.TextChanged += new System.EventHandler(this.RuanTextBox_TextChanged);
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Location = new System.Drawing.Point(9, 47);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(245, 13);
+            this.TitleLabel.TabIndex = 7;
+            this.TitleLabel.Text = "Insira o valor que cada pessoa irá usar (em Reais):";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TitleLabel);
+            this.Controls.Add(this.RuanTextBox);
+            this.Controls.Add(this.CarlosTextBox);
+            this.Controls.Add(this.AlmiraTextBox);
+            this.Controls.Add(this.GroupBoxOfResults);
+            this.Controls.Add(this.RuanLabel);
+            this.Controls.Add(this.CarlosAlbertoLabel);
+            this.Controls.Add(this.AlmiraLabel);
+            this.Name = "Form1";
+            this.Text = "Calculadora de Despesas";
+            this.GroupBoxOfResults.ResumeLayout(false);
+            this.GroupBoxOfResults.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label AlmiraLabel;
+        private System.Windows.Forms.Label CarlosAlbertoLabel;
+        private System.Windows.Forms.Label RuanLabel;
+        private System.Windows.Forms.GroupBox GroupBoxOfResults;
+        private System.Windows.Forms.TextBox AlmiraTextBox;
+        private System.Windows.Forms.TextBox CarlosTextBox;
+        private System.Windows.Forms.TextBox RuanTextBox;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Label TotalOfVariableSpendingsTextBox;
+        private System.Windows.Forms.Label TotalOfFixedSpendingsTextBox;
+        private System.Windows.Forms.Label TotalOfVariableSpendingsLabel;
+        private System.Windows.Forms.Label TotalOfFixedSpendingsLabel;
+        private System.Windows.Forms.Label TotalSpendings;
+        private System.Windows.Forms.Label TotalSpendingsLabel;
+        private System.Windows.Forms.Label TotalIncome;
+        private System.Windows.Forms.Label TotalIncomeLabel;
+    }
+}
+
---
+++ CalculadoraDeDespesas/Utils/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculadoraDeDespesas.Utils
+{
+    public static class Extensions
+    {
+        public static bool CanBeParsedToANumber(this string str)
+        {
+            if (!string.IsNullOrWhiteSpace(str))
+                return double.TryParse(str, out double result);
+            else
+                return false;
+        }
+
+        public static string ParseNumberToBrazilianCurrencyFormat(this double str)
+        {
+            return $"R$ {str.ToString("F2", CultureInfo.CurrentCulture)}";
+        }
+    }
+}
